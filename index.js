d67c7f334389c33bc4b3c51cbafb9426a77b2f62
Chris Hsiao <chsiao76@gmail.com>
2020-05-12 15:36:34 -0700

    displayVideoResults update

--- index.js
+++ index.js
@@ -11,23 +11,24 @@ function watchForm() {
   }
 
 function getTask() {
-  fetch('http://www.boredapi.com/api/activity/')
+  console.log('getTask');
+  fetch('https://www.boredapi.com/api/activity/')
     .then(response => {
         if (response.ok) {
           return response.json();
         }
         throw new Error(response.statusText);
     })
-
     .then(responseJson => 
       displayTaskResults(responseJson))
     .catch(err => {
+      console.dir(err);
       $('#js-taskError-message').text("Oh no something went wrong!! Looks like you'll be bored forever :(");
     });
   };
 
 function displayTaskResults(responseJson) {
-  console.log(responseJson);
+  //console.log(responseJson);
   $('.results-list').empty();
   $('.results-list').append(
     '<h3>'+responseJson.activity+'</h3>',
@@ -43,53 +44,32 @@ function displayTaskResults(responseJson) {
   getVideos(searchPhrase);
 };
 
-/*function formatQueryParams(params) {
-  const queryItems = Object.keys(params)
-    .map(key => '${encodeURIComponent(key)}=${encodeURIComponent(params[key])}')
-  return queryItems.join('&');
-} ;
-
 function getVideos(searchPhrase) {
-  const params = {
-    part: snippet,
-    api_key: AIzaSyBcXbNUU1Q8RmBsUGBaUlVoHCXpp00p2Qo,
-    q: searchPhrase,
-    maxResults: 5,
-  };
-  const queryString = formatQueryParams(params) ;
-  console.log(queryString) ;
-  const url = 'https://www.googleapis.com/youtube/v3/search'+ '?' + queryString;
-  console.log(url);
-};
-*/
-
-function getVideos(searchPhrase) {
-  const url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=' + searchPhrase.split(' ').join('') + '&key=AIzaSyBcXbNUU1Q8RmBsUGBaUlVoHCXpp00p2Qo' ;
-  console.log(url) ;
+  const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=${searchPhrase.replace(/ /g, '+')}&key=AIzaSyCUdJhw03j8wjwAgOig3eOa829KigfWPTs`
   fetch(url)
     .then(response => {
+      console.log(`Success hitting youtube api`);
       if (response.ok) {
         return response.json();
       }
       throw new Error(response.statusText);
     })
-
     .then(responseJson => 
       displayVideoResults(responseJson))
     .catch(err => {
+      console.dir(err);
       $('#js-videoError-message').text("No videos available");
     });
 };
 
 
 function displayVideoResults(responseJson) {
-  console.log(responseJson) ;
-  console.log(responseJson.items[0].id.videoId);
   $('.video-results-list').empty();
-  for (i = 0; i < 6; i++) {
+  for (let i = 0; i < 5; i++) {
     $('.video-results-list').append(
-      '<li><a href=' + '"http://www.youtube.com/watch?v=' + responseJson.items[i].id.videoId + '">Video</a></li>,'
-    )}; 
+      '<li><a href=' + '"http://www.youtube.com/watch?v=' + responseJson.items[i].id.videoId + '">Video ' + [i+1] + '</a></li>'
+    ); 
+  }
 };
 
 
@@ -98,5 +78,3 @@ $(function() {
   console.log('App loaded! Waiting for submit!');
   watchForm();
 });
-
-
